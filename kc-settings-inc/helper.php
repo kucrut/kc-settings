c19453dfc4305eb09bd02642899bb7361fa39113
Dzikri Aziz <kucrut@kucrut.org>
2011-10-26 23:54:38 +0700

    Deprecate kc_dropdown_options() and kcs_select()

--- kc-settings-inc/helper.php
+++ kc-settings-inc/helper.php
@@ -85,44 +85,6 @@ function kc_array_remove_empty( $arr, $rm_zero = true ) {
 
 
 /**
- * Dropdown options (pages/categories), to be used in the theme settings page
- *
- * @param $opt_id ID for the select element
- * @param $type Optional. pages or categories
- *
- * @return dropdown menu
- */
-
-function kc_dropdown_options( $args = array() ) {
-	$defaults =  array(
-		'prefix'	=> 'kc',
-		'mode'		=> 'pages',
-		'section'	=> '',
-		'id'			=> 'pages'
-	);
-
-	$r = wp_parse_args( $args, $defaults );
-	extract( $r, EXTR_SKIP );
-
-	$kc_settings = get_option( "{$prefix}_settings" );
-
-	$dd_args = array(
-		'echo'							=> 0,
-		'hide_empty'				=> 0,
-		'hierarchical'			=> 1,
-		'id'								=> "{$section}__{$id}",
-		'name'							=> "{$prefix}_settings[{$section}][{$id}]",
-		'show_option_none'	=> '&mdash;'.__('Select').'&mdash;'
-	);
-
-	if ( is_array($kc_settings) && isset($kc_settings[$section][$id]) )
-		$dd_args['selected'] = $kc_settings[$section][$id];
-
-	return ( $mode == 'pages' ) ? wp_dropdown_pages( $dd_args ) : wp_dropdown_categories( $dd_args );
-}
-
-
-/**
  * Get theme option
  *
  * @param string $group (Optional) Theme options group, default null
@@ -181,32 +143,6 @@ function kcs_check_roles( $roles = array() ) {
 	return $allowed;
 }
 
-
-
-function kcs_select($arr = array(), $val, $atts = array(), $echo = true) {
-	if ( empty($arr) )
-		return false;
-
-	//$current = ( empty($val) ) ?
-
-	$output  = "<select";
-	if ( !empty($atts) )
-		foreach ( $atts as $k => $v )
-			$output .= " {$k}='".esc_attr($v)."'";
-	$output .= ">\n";
-	foreach ( $arr as $i ) {
-		$output .= "\t<option value='".esc_attr($i['value'])."'";
-		if ( ($val == $i['value'] ) || (empty($val) && isset($i['default']) && $i['default'] === true) )
-			$output .= " selected='selected'";
-		$output .= ">{$i['label']}</option>\n";
-	}
-	$output .= "</select>\n";
-
-	if ( $echo )
-		echo $output;
-	else
-		return $output;
-}
 
 
 function kc_get_taxonomies( $args = array('public' => true) ) {
