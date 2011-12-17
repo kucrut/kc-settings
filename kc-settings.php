669fb3fe82442d4c956d98ed6c64d26821832358
Dzikri Aziz <kucrut@kucrut.org>
2011-12-18 07:31:47 +0800

    Version bump: 2.5.2

--- kc-settings.php
+++ kc-settings.php
@@ -2,7 +2,7 @@
 
 /**
  * @package KC_Settings
- * @version 2.5.1
+ * @version 2.5.2
  */
 
 
@@ -10,7 +10,7 @@
 Plugin name: KC Settings
 Plugin URI: http://kucrut.org/2010/10/kc-settings/
 Description: Easily create plugin/theme settings page, custom fields metaboxes, term meta and user meta settings.
-Version: 2.5.1
+Version: 2.5.2
 Author: Dzikri Aziz
 Author URI: http://kucrut.org/
 License: GPL v2
@@ -620,7 +620,7 @@ function kcSettings_activate() {
 		$status['kids'] = array();
 
 	$old_version = ( isset($status['version']) ) ? $status['version'] : '2.2';
-	$status['version'] = '2.5.1';
+	$status['version'] = '2.5.2';
 
 	update_option( 'kc_settings', $status );
 
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: http://kucrut.org/
 Tags: theme-options, plugin-options, settings, options, term-meta, category-meta, post-meta, custom-fields, user-meta, attachment
 Requires at least: 3.2.1
 Tested up to: 3.3
-Stable tag: 2.5.1
+Stable tag: 2.5.2
 
 Easily create plugin/theme settings page, custom fields metaboxes and term/user meta settings.
 
@@ -35,6 +35,10 @@ Please find the readme.html file inside the plugin directory for the documentati
 5. Post settings
 
 == Changelog ==
+
+= 2.5.2 =
+* Fixed file query limit
+* Fixed kcSettings::_lock()
 
 = 2.5.1 =
 * Fixed kcSettings::get_data(), props rndbit
