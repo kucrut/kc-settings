7ee1ce46129d31fbf62182c3fd89388778b378ce
Dzikri Aziz <kucrut@kucrut.org>
2012-01-21 11:50:35 +0800

    Version bump: 2.6.2

--- kc-settings.php
+++ kc-settings.php
@@ -2,7 +2,7 @@
 
 /**
  * @package KC_Settings
- * @version 2.6.1
+ * @version 2.6.2
  */
 
 
@@ -10,7 +10,7 @@
 Plugin name: KC Settings
 Plugin URI: http://kucrut.org/kc-settings/
 Description: Easily create plugin/theme settings page, custom fields metaboxes, term meta and user meta settings.
-Version: 2.6.1
+Version: 2.6.2
 Author: Dzikri Aziz
 Author URI: http://kucrut.org/
 License: GPL v2
@@ -637,7 +637,7 @@ function kcSettings_activate() {
 		$status['kids'] = array();
 
 	$old_version = ( isset($status['version']) ) ? $status['version'] : '2.2';
-	$status['version'] = '2.6.1';
+	$status['version'] = '2.6.2';
 
 	update_option( 'kc_settings', $status );
 
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: http://kucrut.org/
 Tags: theme-options, plugin-options, settings, options, term-meta, category-meta, post-meta, custom-fields, user-meta, attachment
 Requires at least: 3.3
 Tested up to: 3.3
-Stable tag: 2.6.1
+Stable tag: 2.6.2
 
 Easily create plugin/theme settings pages, custom fields metaboxes and term/user metadata settings.
 
@@ -37,6 +37,9 @@ Please find the readme.html file inside the plugin directory for the documentati
 5. Post settings
 
 == Changelog ==
+
+= 2.6.2 =
+* Fixed ajax in single file field
 
 = 2.6.1 =
 * Cosmetic fixes for single file fields
