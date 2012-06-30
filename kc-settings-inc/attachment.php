803eb15329cf80c3a164a8ab2aa0757cee5eb9b0
Dzikri Aziz <kucrut@kucrut.org>
2012-07-01 06:55:07 +0700

    Restrict WP Editor for attachments metadata to only use QuickTags

--- kc-settings-inc/attachment.php
+++ kc-settings-inc/attachment.php
@@ -2,6 +2,7 @@
 
 class kcSettings_attachment {
 	protected static $settings;
+	public static $sns_done = false;
 
 
 	public static function init( $settings ) {
@@ -10,6 +11,7 @@ class kcSettings_attachment {
 		kcSettings::add_page( 'media.php' );
 		kcSettings::add_page( 'media-upload-popup' );
 
+		add_action( 'admin_head', array(__CLASS__, 'sns'), 9 );
 		add_filter( 'attachment_fields_to_edit', array(__CLASS__, '_edit'), 10, 2 );
 		add_filter( 'attachment_fields_to_save', array(__CLASS__, '_save'), 10, 2 );
 	}
@@ -31,7 +33,7 @@ class kcSettings_attachment {
 				$nu_field = array(
 					'label' => $field['title'],
 					'input' => 'html',
-					'html'  => _kc_field( $input_args )
+					'html'  => ($field['type'] === 'editor') ? self::field_editor( $field['id'], $post->ID, get_post_meta($post->ID, $field['id'], true) ) : _kc_field( $input_args )
 				);
 				if ( isset($desc) && !empty($desc) )
 					$nu_field['helps'] = $field['desc'];
@@ -51,6 +53,41 @@ class kcSettings_attachment {
 		}
 
 		return $post;
+	}
+
+
+	public static function field_editor( $field_id, $post_id, $db_value ) {
+		ob_start();
+		wp_editor(
+			$db_value,
+			"attachments{$post_id}{$field_id}",
+			array(
+				'textarea_name' => "attachments[{$post_id}][{$field_id}]",
+				'textarea_rows' => 7,
+				'media_buttons' => false,
+				'teeny'         => true,
+				'tinymce'       => false,
+				'editor_class'  => 'kcss',
+				'wpautop'       => false,
+				'quicktags'     => array( 'buttons' => 'strong,em,link,block,ul,ol,li,code,ins,del,close' )
+			)
+		);
+
+		return ob_get_clean();
+	}
+
+
+	public static function sns() {
+		global $pagenow;
+		if ( !in_array( $pagenow, array('media-upload.php', 'media.php') ) )
+			return;
+
+		wp_enqueue_script( 'quicktags' ); ?>
+
+<script>window.tinymce = false;</script>
+<style>.wp-editor-wrap {width:460px} .wp-editor-container textarea.wp-editor-area.kcss {width:457px} #media-upload .wp-editor-area {border:0;margin:0}</style>
+	<?php
+		self::$sns_done = true;
 	}
 }
 
