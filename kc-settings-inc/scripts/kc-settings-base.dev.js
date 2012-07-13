f5f1fac2270a2208556d244dd4d151639bb343ec
Dzikri Aziz <kucrut@kucrut.org>
2012-07-13 11:13:51 +0700

    JS: Array.prototype.unique

--- kc-settings-inc/scripts/kc-settings-base.dev.js
+++ kc-settings-inc/scripts/kc-settings-base.dev.js
@@ -2,6 +2,21 @@
  * KC Settings Base
  */
 
+
+// Credit: http://stackoverflow.com/questions/1584370/how-to-merge-two-arrays-in-javascript
+Array.prototype.unique = function() {
+	var a = this.concat();
+	for(var i=0; i<a.length; ++i) {
+		for(var j=i+1; j<a.length; ++j) {
+			if(a[i] === a[j])
+				a.splice(j, 1);
+		}
+	}
+
+	return a;
+};
+
+
 function kcCountObj( obj ) {
 	var count = 0;
 	for (var k in obj) {
