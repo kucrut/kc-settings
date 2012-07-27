307681f698b002462f02157fbc75252062a535e9
Dzikri Aziz <kucrut@kucrut.org>
2012-07-28 00:13:05 +0700

    Added link to GitHub issues page for feature request on the contextual help

--- kc-settings-inc/builder.php
+++ kc-settings-inc/builder.php
@@ -203,6 +203,7 @@ class kcSettings_builder {
 					'<ul>
 						<li><a href="http://kucrut.github.com/kc-settings/">'.__('Online documentation', 'kc-settings').'</a></li>
 						<li><a href="http://wordpress.org/tags/kc-settings?forum_id=10">'.__('Support', 'kc-settings').'</a></li>
+						<li><a href="https://github.com/kucrut/kc-settings/issues">'.__('Request new feature', 'kc-settings').'</a></li>
 						<li><a href="http://kucrut.org/contact/">'.__('Contact', 'kc-settings').'</a></li>
 					</ul>'
 			)
