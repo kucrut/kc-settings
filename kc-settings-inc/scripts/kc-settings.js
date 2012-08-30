4265aaa314b3794347cea59653097ec94a711ef0
Dzikri Aziz <kvcrvt@gmail.com>
2012-08-31 04:14:22 +0700

    Fixed JS loader and field styles

--- kc-settings-inc/scripts/kc-settings.js
+++ kc-settings-inc/scripts/kc-settings.js
@@ -49,7 +49,7 @@ jQuery(document).ready(function($) {
 						inst.dpDiv.removeClass('kcDPMonth');
 					}, 300);
 				},
-				currentText: kcSettings.texts.now
+				currentText: win.kcSettings.texts.now
 			},
 			datetime: {
 				dateFormat: 'yy-mm-dd',
@@ -57,24 +57,24 @@ jQuery(document).ready(function($) {
 				separator: 'T',
 				stepHour: 1,
 				stepMinute: 1,
-				currentText: kcSettings.texts.now
+				currentText: win.kcSettings.texts.now
 			},
 			text: {
 				isRTL: win.isRTL,
-				timeText: kcSettings.texts.time,
-				hourText: kcSettings.texts.hour,
-				minuteText: kcSettings.texts.minute,
-				currentText: kcSettings.texts.today,
-				closeText: kcSettings.texts.done,
-				nextText: kcSettings.texts.next,
-				prevText: kcSettings.texts.prev,
-				dayNames: kcSettings.texts.dayNames.full,
-				dayNamesMin: kcSettings.texts.dayNames.min,
-				dayNamesShort: kcSettings.texts.dayNames.shrt,
-				monthNames: kcSettings.texts.monthNames.full,
-				monthNamesShort: kcSettings.texts.monthNames.shrt,
-				weekHeader: kcSettings.texts.weekNames.shrt,
-				timeOnlyTitle: kcSettings.texts.chooseTime
+				timeText: win.kcSettings.texts.time,
+				hourText: win.kcSettings.texts.hour,
+				minuteText: win.kcSettings.texts.minute,
+				currentText: win.kcSettings.texts.today,
+				closeText: win.kcSettings.texts.done,
+				nextText: win.kcSettings.texts.next,
+				prevText: win.kcSettings.texts.prev,
+				dayNames: win.kcSettings.texts.dayNames.full,
+				dayNamesMin: win.kcSettings.texts.dayNames.min,
+				dayNamesShort: win.kcSettings.texts.dayNames.shrt,
+				monthNames: win.kcSettings.texts.monthNames.full,
+				monthNamesShort: win.kcSettings.texts.monthNames.shrt,
+				weekHeader: win.kcSettings.texts.weekNames.shrt,
+				timeOnlyTitle: win.kcSettings.texts.chooseTime
 			}
 		}
 	};
@@ -110,7 +110,7 @@ jQuery(document).ready(function($) {
 	var $dateInputs = $('input[type=date], input[type=month]');
 	if ( $dateInputs.length && Modernizr.inputtypes.date === false ) {
 		Modernizr.load([{
-			load: kcGetSNS('jquery_ui_datepicker', kcSettings.js).concat( kcGetSNS('jquery_ui', kcSettings.css) ),
+			load: kcGetSNS('jquery_ui_datepicker', win.kcSettings.js).concat( kcGetSNS('jquery_ui', win.kcSettings.css) ),
 			complete: function() {
 				$dateInputs.each(function() {
 					$(this).datepicker( $.extend( args.datepicker.text, args.datepicker[$(this).attr('type')] ) );
@@ -122,7 +122,7 @@ jQuery(document).ready(function($) {
 	var $dtInputs = $('input[type=datetime]');
 	if ( $dtInputs.length && Modernizr.inputtypes['datetime'] === false ) {
 		Modernizr.load([{
-			load: kcGetSNS('jquery_ui_datetimepicker', kcSettings.js),
+			load: kcGetSNS('jquery_ui_datetimepicker', win.kcSettings.js),
 			complete: function() {
 				var _conf = $.extend( args.datepicker.text, args.datepicker.datetime );
 				$dtInputs.datetimepicker( $.extend( _conf, {timeFormat: 'hh:mmZ'} ) );
@@ -133,7 +133,7 @@ jQuery(document).ready(function($) {
 	var $dtlInputs = $('input[type=datetime-local]');
 	if ( $dtlInputs.length && Modernizr.inputtypes['datetime-local'] === false ) {
 		Modernizr.load([{
-			load: kcGetSNS('jquery_ui_datetimepicker', kcSettings.js),
+			load: kcGetSNS('jquery_ui_datetimepicker', win.kcSettings.js),
 			complete: function() {
 				$dtlInputs.datetimepicker( $.extend( args.datepicker.text, args.datepicker.datetime ) );
 			}
@@ -144,7 +144,7 @@ jQuery(document).ready(function($) {
 	var $timeInputs = $('input[type=time]');
 	if ( $timeInputs.length && Modernizr.inputtypes['time'] === false ) {
 		Modernizr.load([{
-			load: kcGetSNS('jquery_ui_datetimepicker', kcSettings.js),
+			load: kcGetSNS('jquery_ui_datetimepicker', win.kcSettings.js),
 			complete: function() {
 				$timeInputs.timepicker( $.extend( args.datepicker.text, args.datepicker.datetime ) );
 			}
@@ -155,7 +155,7 @@ jQuery(document).ready(function($) {
 	var $colorInputs = $('input[type=color]');
 	if ( $colorInputs.length && Modernizr.inputtypes.color === false ) {
 		Modernizr.load([{
-			load: kcGetSNS( 'jquery_colorpicker', kcSettings.js).concat( kcGetSNS('jquery_colorpicker', kcSettings.css) ),
+			load: kcGetSNS( 'jquery_colorpicker', win.kcSettings.js).concat( kcGetSNS('jquery_colorpicker', win.kcSettings.css) ),
 			complete: function () {
 				$colorInputs.ColorPicker(args.colorpicker)
 				.each(function() {
